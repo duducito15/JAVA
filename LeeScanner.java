ed46a438d7b5f4aa8befccd4a116c11fae342a3e
Eduardo Chavez <echavezv@utea.edu.pe>
2023-08-28 11:45:32 -0500

    Scanner

---
+++ LeeScanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LeeScanner{
+    public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+      
+       System.out.println("Ingrese un nombre: ");
+       String nombre = s.nextLine();
+
+       System.out.println("Ingrese tu edad: ");
+       int edad = Integer.parseInt(s.nextLine());
+
+       System.out.println("Hola "+nombre+" tu edad es: "+edad);
+        
+    }
+}
