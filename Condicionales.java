5aeadf1a74b55a473241f5ea641005a6f5a2507d
Eduardo Chavez <echavezv@utea.edu.pe>
2023-09-04 11:40:46 -0500

    Condicionales

---
+++ Condicionales.java
@@ -0,0 +1,87 @@
+public class Condicionales {
+    public static void main(String[] args) {
+        // Sentencia condicional
+        /*
+         * Es una sentenciaque permite bifurcar la ejecucion
+         * del programa dependiendo de un valor o expresion
+         * booleana (verdad o falso)
+         * 
+         * if
+         * esta sentencia permite la ejecución de una serie
+         * de instrucciones en funcion al resultado de una
+         * expresion logica. expresion logica puede retornarme verdad
+         * o falso.
+         */
+        /*
+         * String fruta = "mango";
+         * 
+         * if ("mango".equals(fruta)) {
+         * System.out.println("Iguales");
+         * } else {
+         * System.out.println("distintas");
+         * }
+         */
+        /*
+         * String respuesta;
+         * System.out.println("¿Cuál es la capital del Perú?");
+         * respuesta = System.console().readLine();
+         * 
+         * if (respuesta.equals("Lima")) {
+         * System.out.println("Su respuesta es correcta");
+         * } else {
+         * System.out.println("Su respuesta es incorrecta");
+         * }
+         */
+
+        /*
+         * int numero;
+         * System.out.println("Por favor, introduce un número entero:");
+         * numero = Integer.parseInt(System.console().readLine());
+         * /*Operadores de comparación
+         * == igual a == b
+         * != distinto a != b
+         * < menor a < b
+         * <= menor igual a <= b
+         * > mayor a > b
+         * >= mayor igual a >= b
+         * 
+         * if (numero > 0) {
+         * System.out.println("El número introducido es positivo");
+         * } else {
+         * System.out.println("El número introducido es negativo");
+         * }
+         */
+
+        /*int nota;
+        System.out.println("Por favor, ingresa la nota que has sacado:");
+        nota = Integer.parseInt(System.console().readLine());
+        if (nota >= 11) {
+            System.out.println("¡Has aprobado!");
+        } else {
+            System.out.println("Lo sienteo, has desaprobado");
+        }
+        
+        Operadores lógicos
+        &&      y       ((7 > 2) && (2 < 4)) => verdad
+        ||      o       ((7 > 2) || (2 > 4)) => verdad
+        !       no      !(7 > 2) => falso
+       */
+      System.out.println("Adivina el numero que estoy pensando");
+      System.out.println("Introduce un numero entre el 1 y el 50");
+      int n;
+      n= Integer.parseInt(System.console().readLine());
+
+      if ((n < 1)|| (n > 50)) {
+        System.out.println("El numero debe estar entre 1 - 50");
+        System.out.println("Tienes otra oportunidad, ingrese un número: ");
+         n= Integer.parseInt(System.console().readLine());
+      }
+
+      if (n==20) {
+        System.out.println("¡Has acertado!");
+      } else{
+        System.out.println("Lo siento, ese no es el numero que estoy pensando");
+      }
+
+    }
+}
