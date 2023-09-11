adb06df348c01a79d92e2a017fbba9dd8161de45
Eduardo Chavez <echavezv@utea.edu.pe>
2023-09-11 11:26:16 -0500

    Bucle For

---
+++ Bucles.java
@@ -0,0 +1,22 @@
+public class Bucles {
+    public static void main(String[] args) {
+        // Los bucles se usan para repetir un conjunto de instrucciones
+        // para poder salir de un bucle debe haber una condicion de salida
+        // Bucle for: se usa siempre y cuando se sepa previamente cuantas repeticioens
+        // voy a realizar
+        for (int i = 0; i < 101; i = i + 5) {
+            // instrucciones a repetir
+            /*
+             * if (i % 5 == 0) {
+             * System.out.println(i);
+             * }
+             */
+            System.out.println(i);
+        }
+
+        // Muestre los numeros multiplos de 5 que hay entre 0 a 100
+        // Muestra los numeros del 320 al 160, contando de 20 en 20
+        // hacia atras
+
+    }
+}
