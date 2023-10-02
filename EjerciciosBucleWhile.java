e8841136248427119f70202bcc7ad09a51f712a5
Eduardo Chavez <echavezv@utea.edu.pe>
2023-10-02 12:28:16 -0500

    Ejercicio 2 While

--- EjerciciosBucleWhile.java
+++ EjerciciosBucleWhile.java
@@ -1,23 +1,31 @@
 public class EjerciciosBucleWhile {
     // Ejericio 2.
     public static void main(String[] args) {
-        // Ejericio 1.
-        int intentos = 4;
-        int numeroIntroducido;
-        boolean acertado = false;
+        // Ejericio 2
+        int numeroIntroducido = 0;
+        int cuentaNumero = 0;
+        int maximoPar = 0;
+        int sumaImpar = 0;
+        int numerosImpares = 0;
 
-        while ( (intentos > 0) && (!acertado) ) {
-            System.out.println("Introducir la clave de la caja fuerte: ");
+        while (numeroIntroducido >= 0) {
             numeroIntroducido = Integer.parseInt(System.console().readLine());
-
-            if (numeroIntroducido == 1234) {
-                System.out.println("Ha abierto la caja fuerte");
-                acertado = true;
-               // break;
-            } else {
-                System.out.println("Clave incorrecta");
+            cuentaNumero++;
+            if (numeroIntroducido >= 0) {
+                if (numeroIntroducido % 2 == 0) {
+                    if (numeroIntroducido > maximoPar) {
+                        maximoPar = numeroIntroducido;
+                    }
+                } else {
+                    sumaImpar = sumaImpar + numeroIntroducido;
+                    numerosImpares++;
+                }
             }
-            intentos--;
         }
+
+        System.out.println("Ha introducido " + (cuentaNumero - 1) + " de elementos");
+        System.out.println("La media de los impares es " + sumaImpar / numerosImpares);
+        System.out.println("El mayor par es " + maximoPar);
+
     }
 }
