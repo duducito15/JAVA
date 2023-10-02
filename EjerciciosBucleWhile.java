fe7e83ab43584a337a0d904236e265285266c110
Eduardo Chavez <echavezv@utea.edu.pe>
2023-10-02 12:09:18 -0500

    ejercicio 1

--- EjerciciosBucleWhile.java
+++ EjerciciosBucleWhile.java
@@ -1,3 +1,23 @@
 public class EjerciciosBucleWhile {
-    
+    // Ejericio 2.
+    public static void main(String[] args) {
+        // Ejericio 1.
+        int intentos = 4;
+        int numeroIntroducido;
+        boolean acertado = false;
+
+        while ( (intentos > 0) && (!acertado) ) {
+            System.out.println("Introducir la clave de la caja fuerte: ");
+            numeroIntroducido = Integer.parseInt(System.console().readLine());
+
+            if (numeroIntroducido == 1234) {
+                System.out.println("Ha abierto la caja fuerte");
+                acertado = true;
+               // break;
+            } else {
+                System.out.println("Clave incorrecta");
+            }
+            intentos--;
+        }
+    }
 }
